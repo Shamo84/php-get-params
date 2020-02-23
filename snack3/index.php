cc36b3cbc44b447e1bc392915692033d00b9aac9
Andrea Pacifico <pacifico.mensa@gmail.com>
2020-02-23 16:56:38 +0100

    up

--- snack3/index.php
+++ snack3/index.php
@@ -3,18 +3,9 @@
 <?php
   $paragraph = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
   $array = explode (". ", $paragraph);
-  var_dump($array);
-  
 
+  for ($i=0; $i < count($array); $i++) {
+    $string .= "<p>" . $array[$i] . "</p>";
+  }
+  echo $string;
 ?>
-
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Paragraph</title>
-  </head>
-  <body>
-    <?php var_dump $array; ?>
-  </body>
-</html>
